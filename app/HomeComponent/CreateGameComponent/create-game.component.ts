32ec89ee4c60b4b11e0e27e43ff4676a7b90a80c
SomePeaceStudio <innuss22@gmail.com>
2016-03-04 14:44:39 +0200

    Kick and Ban event Mock Data restructured

--- app/HomeComponent/CreateGameComponent/create-game.component.ts
+++ app/HomeComponent/CreateGameComponent/create-game.component.ts
@@ -6,99 +6,71 @@ import { HostCardComponent } from './host-card.component';
   directives: [PlayerCardComponent,HostCardComponent]
 })
 
-
 export class CreateGameComponent {
-  constructor() {
-
-  }
-  public me: Player = {id:1,
-                  name: 'John Doe',
-                  username: 'John Doe',
-                  imageUrl: ''};
-  public one: Player = {id:2,
-                  name: 'Gorge Hammond',
-                  username: 'Gorge Hammond',
-                  imageUrl: ''};
-  public two: Player = {id:3,
-                  name: 'Peter Smith',
-                  username: 'Peter Smith',
-                  imageUrl: ''};
-
+  constructor() { }
+  public banList: BannedPlayer[] = [];
+  public me: Player = MOCKME;
+  public players: Player[] = MOCKPLAYERS;
   public submited: boolean = false;
+
   submitGameLabel(field){
     field.disabled = true;
     this.submited = true;
-  //  this.addSelf();
-  //  this.debugStart();
-  //  this.getPlayers();
   }
-  addSelf(){
-    this.players.push(this.me);
-    // generate dom element
-
+  onKick(event, toast){
+    this.players.forEach((player, i)=>{
+      if(player.id === event.id){
+        this.players.splice(i, 1);
+        toast.text = player.username + " has been kicked";
+        toast.open();
+      }
+    });
   }
-  public players: Player[]=[this.one,this.two];
-  /*
-  addPlayer(newPlayer : Player){
-    this.players.push(newPlayer);
-    //Generate dom element
-    var list = document.getElementById('player-list');
-    var card = `<paper-material class="player-lobby-card center-horizontal" id="`+newPlayer.username+`">
-        <img src="`+newPlayer.imageUrl+`" class="avatar-small center-vertical">
-        <p class="center-vertical">`+newPlayer.username+`</p>
-        <paper-menu-button>
-          <paper-icon-button icon="more-vert" class="dropdown-trigger"></paper-icon-button>
-          <paper-menu varticalAlign='top' class="dropdown-content layout vertical">
-            <paper-item (click)="kick('`+newPlayer.username+`',toast)">Kick</paper-item>
-            <paper-item #item (click)="ban(`+newPlayer.username+`,toast)">Ban</paper-item>
-          </paper-menu>
-        </paper-menu-button>
-    </paper-material>
-    `
-    angular.element(list).append(card);
-    // list.appendChild(card);
+  onBan(event, toast){
+    this.players.forEach((player, i)=>{
+      if(player.id === event.id){
+        this.players.splice(i, 1);
+        this.banList.push(this.convertToBanned(player));
+        toast.text = player.username + " has been banned for 5 minutes";
+        toast.open();
+      }
+    });
   }
-
-
-  /*
-  addPlayer(newPlayer : Player){}
-*/
-  // for now 'name' later on use player ID for identification
-  removePlayer(name){
-    var card = document.getElementById(name);
-    // card.content = "";
-    return true;
-  }
-
-  kick(name,toast){
-    toast.close();
-    toast.text = name + " has been kicked";
-    toast.open();
-    this.removePlayer(name);
-    return true;
-  }
-
-  onKick(eventObj){
-    console.log("kick Event");
-    console.log(JSON.stringify(eventObj.username));
-  }
-
-  ban(name,toast){
-    toast.close();
-    toast.text = name + " has been banned for 5 minutes";
-    toast.open();
-    this.removePlayer(name);
-    return true;
+  convertToBanned(player){
+    let bannedPlayer: BannedPlayer = {
+      name: player.name,
+      username: player.username,
+      banTime: new Date()
+    }
+    return bannedPlayer;
   }
 
   get list(){
     return JSON.stringify(this.players);
   }
+  get bans(){
+    return JSON.stringify(this.banList);
+  }
 }
-
+export var MOCKME: Player = {
+                id:1,
+                name: 'John Doe',
+                username: 'John Doe',
+                imageUrl: ''
+};
+export var MOCKPLAYERS: Player[] = [
+  {id:2, name: 'George Hammond', username: 'George Hammond', imageUrl: ''},
+  {id:3, name: 'Jenny Frey', username: 'Zebiegste', imageUrl: ''},
+  {id:4, name: 'Roberto Benini', username: 'RobertoGuy', imageUrl: ''}
+]
 export interface Player{
   id: number,
   name: string,
   username: string,
   imageUrl: string
+}
+export interface BannedPlayer{
+  name: string,
+  username: string,
+  banTime: Date
 }
--- app/HomeComponent/CreateGameComponent/player-card.component.ts
+++ app/HomeComponent/CreateGameComponent/player-card.component.ts
@@ -2,16 +2,23 @@ import { Component, Input, Output, EventEmitter } from 'angular2/core';
 import { Player } from './create-game.component';
 @Component({
   selector: 'player-card',
-  templateUrl: 'app/HomeComponent/CreateGameComponent/player-card.component.html',
-  events: [ 'kick' ]
+  templateUrl: 'app/HomeComponent/CreateGameComponent/player-card.component.html'
 })
 export class PlayerCardComponent {
   constructor() { }
   @Input() player: Player;
-  @Output() kick = new EventEmitter;
-  kickClick($event, username){
-    console.log("childEmiter!")
-    this.kick.next({event: $event,username: username});
+  @Output() kick = new EventEmitter();
+  @Output() ban = new EventEmitter();
+
+  kickClick(player){
+    this.kick.next({
+      id: player.id
+    });
+  }
+  banClick(player){
+    this.ban.next({
+      id: player.id
+    });
   }
   //remove for pruduction
   get debug(){
